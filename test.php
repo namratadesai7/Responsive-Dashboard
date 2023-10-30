ac432282b004927d28243c5830f11a5b45450bb2
Namrata Desai <namratadesai003@gmail.com>
2023-10-30 18:35:32 +0530

    First-commit

---
+++ test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Position tutorial</title>
+    <style>
+        div{
+            hea
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <div></div>
+        <div class="static"></div>
+    </div>
+    
+</body>
+</html>
